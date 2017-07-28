85b4a89ebdfbd13fdd90a614d3299dc695290140
John Howard <longhornjoho@gmail.com>
2017-07-28 03:18:22 +0000

    create seeds file for sample data

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170728024815) do
   create_table "portfolios", force: :cascade do |t|
     t.string   "title"
     t.string   "subtitle"
+    t.text     "body"
     t.text     "main_image"
     t.text     "thumb_image"
     t.datetime "created_at",  null: false
