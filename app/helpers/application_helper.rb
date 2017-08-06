ce3b8b923affe63efb95fd2b0b49e32001fbc9cd
John Howard <longhornjoho@gmail.com>
2017-08-06 15:51:12 +0000

    create content tag helper for source

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,11 @@ module ApplicationHelper
   end
   
   
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]}"
+      content_tag(:p, greeting, class: "source-greeting")
+    end
+  end
+  
 end
