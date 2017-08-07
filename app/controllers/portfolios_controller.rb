2fe418b93eb42925b539c9f96f62e9c7fcdef0d6
John Howard <longhornjoho@gmail.com>
2017-08-07 00:36:54 +0000

    refactored before_action to set portfolio_item variable

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout "portfolios"
   
   def index
@@ -7,7 +8,6 @@ class PortfoliosController < ApplicationController
   end
   
   def show
-     @portfolio_item = Portfolio.find(params[:id])
      @page_title = Portfolio.find(params[:id]).title
   end
   
@@ -29,11 +29,9 @@ class PortfoliosController < ApplicationController
   end
   
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
   end
   
   def update
-    @portfolio_item = Portfolio.find(params[:id])
     
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
@@ -45,7 +43,6 @@ class PortfoliosController < ApplicationController
   end
   
   def destroy
-    @portfolio_item = Portfolio.find(params[:id])
     
     @portfolio_item.destroy
     
@@ -60,6 +57,10 @@ class PortfoliosController < ApplicationController
                                         :subtitle,
                                         :body,
                                         technologies_attributes: [:name])
+    end
+  
+    def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
     end
   
 end
